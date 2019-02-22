a5c4a43c84ca7a9abbd9c2356ec32f0fc5b59f7e
shenxiaojie <17610606362@163.com>
2019-02-23 00:50:16 +0800

    .

---
+++ src/main/java/com/hexin/common/dianIt/tetsss.java
@@ -0,0 +1,13 @@
+package com.hexin.common.dianIt;
+
+
+import com.hexin.entity.User;
+
+public class tetsss {
+
+
+    public static void main(String[] args) {
+        User user = new User();
+
+    }
+}
